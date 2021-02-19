7903ff3202518c8ce83494d99d441fd9a999d7e6
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2021-02-19 12:45:17 -0600

    mkimage: Add a 'keyfile' argument for image signing
    It's not always desirable to use 'keydir' and some ad-hoc heuristics
    to get the filename of the signing key. More often, just passing the
    filename is the simpler, easier, and logical thing to do.
    
    Since mkimage doesn't use long options, we're slowly running out of
    letters. I've chosen '-G' because it was available.
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    (cherry picked from commit 36bfcb62b3e7b846d0b693828df54a0d58e07511)
    [Tan: fix backport getopt conflict in tools/mkimage.c]
    Signed-off-by: Tan Siewert <tan.siewert@9elements.com>

--- tools/mkimage.c
+++ tools/mkimage.c
@@ -101,6 +101,7 @@ static void usage(const char *msg)
 		"          -E => place data outside of the FIT structure\n"
 		"          -k => set directory containing private keys\n"
 		"          -K => write public keys to this .dtb file\n"
+		"          -G => use this signing key (in lieu of -k)\n"
 		"          -c => add comment in signature node\n"
 		"          -F => re-sign existing FIT image\n"
 		"          -p => place external data at a static position\n"
@@ -143,7 +144,7 @@ static void process_args(int argc, char **argv)
 	int opt;
 
 	while ((opt = getopt(argc, argv,
-			     "a:A:b:c:C:d:D:e:Ef:Fk:i:K:ln:N:p:O:rR:qsT:vVx")) != -1) {
+		   "a:A:b:B:c:C:d:D:e:Ef:Fk:FG:k:i:K:ln:N:p:O:rR:qstT:vVx")) != -1) {
 		switch (opt) {
 		case 'a':
 			params.addr = strtoull(optarg, &ptr, 16);
@@ -208,6 +209,9 @@ static void process_args(int argc, char **argv)
 			 */
 			params.type = IH_TYPE_FLATDT;
 			params.fflag = 1;
+			break;
+		case 'G':
+			params.keyfile = optarg;
 			break;
 		case 'i':
 			params.fit_ramdisk = optarg;
